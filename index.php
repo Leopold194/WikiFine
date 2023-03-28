efee90a716ca83b6b18c542b3658f7b066c0e93e
ZACK7870 <zack7870@gmail.com>
2023-03-28 17:52:10 +0200

    update favicon and page title

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset='UTF-8'>
-    <title>Page Title</title>
+    <title>Wikifine</title>
+    <link rel="icon" type="image/x-icon" href="img\logos\wikifineColorFavicon.png">
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' href='css/templates/style.css'>
 
