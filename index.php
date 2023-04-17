d7df2cb0001a886f7b777e5208bad5a00d807a46
Leopold <leopold.goudier@gmail.com>
2023-04-17 18:45:44 +0200

    add login

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         </div>
         <div class="profileBlock">
             <div class="login button">
-                <a href=""><p>Se connecter</p></a>
+                <a href="pages/login.php"><p>Se connecter</p></a>
             </div>
             <div class="register button">
                 <a href="pages/register1.php"><p>S'inscrire</p></a>
