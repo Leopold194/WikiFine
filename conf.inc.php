b032438d193e0de52137d2aa86b5c1c08a04e800
Leopold <leopold.goudier@gmail.com>
2023-04-19 14:41:30 +0200

    update conf.inc

--- conf.inc.php
+++ conf.inc.php
@@ -3,7 +3,7 @@
 $prefix = ($_SERVER['HTTP_HOST'] == 'localhost') ? ("/WikiFine/") : ("/");
 define("FILE_PREFIX", $prefix);
 
-$secret_prefix = ($_SERVER['HTTP_HOST'] == 'localhost') ? ("C:/xampp/htdocs/") : ("/www/");
+$secret_prefix = ($_SERVER['HTTP_HOST'] == 'localhost') ? ("C:/xampp/htdocs/") : ("/var/www/");
 $data = file_get_contents($secret_prefix.'secrets/secrets.json');
 $obj = json_decode($data);
 
