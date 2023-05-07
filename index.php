ecbd994efa0d09e2ca03f80c690674f9deac6637
ZACK7870 <zack7870@gmail.com>
2023-05-07 20:51:53 +0200

    add new home page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
-    session_start();
-    require 'core/functions.php';
+<?php
+session_start();
+require 'core/functions.php';
 ?>
 
 <?php require 'conf.inc.php'; ?>
@@ -9,43 +9,112 @@
 <link rel='stylesheet' href='css/index.css'>
 <?php require 'pages/templates/navbar.php'; ?>
 <?php require 'pages/templates/sidebar.php'; ?>
+<style>
+    .contentIndex {
+        position: relative;
+        width: calc(100% - 350px);
+        margin-left: 350px;
+        margin-right: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 30px;
+    }
 
-    <div class="article">
-        <div class="articleContent">
-            <h1>Le nombre d'Or</h1>
-            <p>Il intervient dans la construction du pentagone régulier. Ses propriétés algébriques le lient à la suite
-                de
-                Fibonacci et au corps quadratique ℚ(√5). Le nombre d'or s'observe aussi dans la nature (quelques
-                phyllotaxies, par exemple chez les capitules du tournesol, pavage de Penrose de quasi-cristaux) ou dans
-                quelques œuvres et monuments (architecture de Le Corbusier, musique de Xenakis, peinture de Dalí).</p>
-            <h2>Lorem Ipsum</h2>
-            <p>L'histoire de cette proportion commence à une période de l'Antiquité qui n'est pas connue avec certitude
-                ; la
-                première mention connue de la division en extrême et moyenne raison apparaît dans les Éléments
-                d'Euclide. À
-                la Renaissance, Luca Pacioli, un moine franciscain italien, la met à l'honneur dans un manuel de
-                mathématiques et la surnomme « divine proportion » en l'associant à un idéal envoyé du ciel. Cette
-                vision se
-                développe et s'enrichit d'une dimension esthétique, principalement au cours des xixe et xxe siècles où
-                naissent les termes de « section dorée » et de « nombre d'or ».
-                Il est érigé en théorie esthétique et justifié par des arguments d'ordre mystique, comme une clé
-                importante,
-                voire explicative, dans la compréhension des structures du monde physique, particulièrement pour les
-                critères de beauté et surtout d'harmonie ; sa présence est alors revendiquée dans les sciences de la
-                nature
-                et de la vie, proportions du corps humain ou dans les arts comme la peinture, l'architecture ou la
-                musique.
-                Certains artistes, tels le compositeur Xenakis ou le poète Paul Valéry ont adhéré à une partie de cette
-                vision, soutenue par des livres populaires.
-                À travers la médecine, l'archéologie ou les sciences de la nature et de la vie, la science infirme les
-                théories de cette nature car elles sont fondées sur des généralisations abusives et des hypothèses
-                inexactes.</p>
+    .welcomeTitle {
+        text-align: center;
+        font-size: 35px;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        margin-bottom: 100px;
+    }
 
+    .recommendationsTitle {
+        text-align: left;
+        font-size: 30px;
+        font-weight: bold;
+        width: 100%;
+    }
+
+    .recommendationFrame {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start; 
+        gap: 20px;
+        width: 100%;
+        margin-bottom: 50px;
+    }
+
+    .recommendationCard {
+        position: relative;
+        width: calc(33.333% - 20px);
+        height: 250px;
+        background: #ECEBEB;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+    }
+    .cardTitle {
+        font-size: 24px;
+        font-weight: bold;
+        margin: 16px 16px;
+        text-align: left;
+    }
+    .cardImage {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        object-fit: cover;
+        position: absolute;
+        top: 16px;
+        right: 16px;
+    }
+    .cardContent {
+        font-size: 16px;
+        text-align: justify;
+        padding: 16px 16px;
+    }
+</style>
+
+
+<body>
+    <div class="contentIndex">
+        <div class="welcomeTitle">
+            <h1>Ravi de votre retour sur Wikifine !</h1>
         </div>
-        <div class="imgBlock">
-            <img class="imgArticle" src="img\articles\spirale.gif" alt="spirale">
-            <p class="imgTitle">Spirale logarithmetique</p><br>
-            <p class="imgSubTitle">by Eduardo Renno in 2018</p>
+        <h1 class="recommendationsTitle">Recommandations</h1>
+        <div class="recommendationFrame">
+            <div class="recommendationCard">
+                <img class="cardImage" src="img/cards_logos/Burj.png" alt="Burj Khalifa">
+                <h2 class="cardTitle">Burj Khalifa</h2>
+                <p class="cardContent">Burj Khalifa fait partie d’un vaste projet urbanistique, immobilier et architectural couvrant une superficie de 2 km2. Il s’agit de créer un nouveau quartier, Downtown Dubai, un peu au sud du centre historique de Dubaï aux Émirats arabes unis...</p>
+            </div>
+            <div class="recommendationCard">
+                <img class="cardImage" src="img/cards_logos/Eiffel.jpg" alt="Tour Eiffel">
+                <h2 class="cardTitle">Tour Eiffel</h2>
+                <p class="cardContent">La Tour Eiffel est une tour de fer puddlé de 324 mètres de hauteur située à Paris, à l’extrémité nord-ouest du parc du Champ-de-Mars en bordure de la Seine dans le 7ᵉ arrondissement...</p>
+            </div>
+            <div class="recommendationCard">
+                <img class="cardImage" src="img/cards_logos/colisee.jpg" alt="Colisée">
+                <h2 class="cardTitle">Le Colisée</h2>
+                <p class="cardContent">Le Colisée, à l'origine amphithéâtre Flavien, est un immense amphithéâtre ovoïde situé dans le centre de la ville de Rome, entre l'Esquilin et le Caelius, le plus grand jamais construit dans l'Empire romain...</p>
+            </div>
+        </div>
+        <h1 class="recommendationsTitle">Les derniers articles publiés</h1>
+        <div class="recommendationFrame">
+            <div class="recommendationCard">
+                <img class="cardImage" src="img/cards_logos/Burj.png" alt="Burj Khalifa">
+                <h2 class="cardTitle">Burj Khalifa</h2>
+                <p class="cardContent">Burj Khalifa fait partie d’un vaste projet urbanistique, immobilier et architectural couvrant une superficie de 2 km2. Il s’agit de créer un nouveau quartier, Downtown Dubai, un peu au sud du centre historique de Dubaï aux Émirats arabes unis...</p>
+            </div>
+            <div class="recommendationCard">
+                <img class="cardImage" src="img/cards_logos/Eiffel.jpg" alt="Tour Eiffel">
+                <h2 class="cardTitle">Tour Eiffel</h2>
+                <p class="cardContent">La Tour Eiffel est une tour de fer puddlé de 324 mètres de hauteur située à Paris, à l’extrémité nord-ouest du parc du Champ-de-Mars en bordure de la Seine dans le 7ᵉ arrondissement...</p>
+            </div>
+            <div class="recommendationCard">
+                <img class="cardImage" src="img/cards_logos/colisee.jpg" alt="Colisée">
+                <h2 class="cardTitle">Le Colisée</h2>
+                <p class="cardContent">Le Colisée, à l'origine amphithéâtre Flavien, est un immense amphithéâtre ovoïde situé dans le centre de la ville de Rome, entre l'Esquilin et le Caelius, le plus grand jamais construit dans l'Empire romain...</p>
+            </div>
         </div>
     </div>
 </body>
