feba7cf601014bfbdc58cb9fa7e6001fd4677383
Léopold Goudier <46968525+Leopold194@users.noreply.github.com>
2023-06-30 17:29:56 +0200

    Update sidebar.php

--- pages/templates/sidebar.php
+++ pages/templates/sidebar.php
@@ -3,7 +3,7 @@
         <a href=<?php echo FILE_PREFIX."index.php";?>><li class="navTitle">Accueil</li><br></a>
         <li class="navSubTitle">Portails thématiques</li>
         <a href=<?php echo FILE_PREFIX."core/articles/random_article.php";?>><li class="navSubTitle">Article au hasard</li></a>
-        <li class="navSubTitle">Contact</li>
+        <a href=<?php echo FILE_PREFIX."pages/contact.php";?>><li class="navSubTitle">Contact</li>
         <li class="navTitle">Contribuer</li><br>
         <li class="navSubTitle">Débuter sur Wikifine</li>
         <li class="navSubTitle">Aide</li>
