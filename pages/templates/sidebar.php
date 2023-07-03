aa906b6b08a8cc164021cf71973e6bcc2e80fb3e
yb <ylianbsila22@gmail.com>
2023-07-03 23:24:57 +0200

    link-help-page

--- pages/templates/sidebar.php
+++ pages/templates/sidebar.php
@@ -5,10 +5,7 @@
         <a href=<?php echo FILE_PREFIX."core/articles/random_article.php";?>><li class="navSubTitle">Article au hasard</li></a>
         <a href=<?php echo FILE_PREFIX."pages/contact.php";?>><li class="navSubTitle">Contact</li></a>
         <li class="navTitle">Contribuer</li><br>
-        <li class="navSubTitle">Débuter sur Wikifine</li>
-        <li class="navSubTitle">Aide</li>
-        <li class="navSubTitle">Communauté</li>
-        <li class="navSubTitle">Modifications récentes</li>
+        <a href=<?php echo FILE_PREFIX."pages/aide.php";?>><li class="navSubTitle">Débuter sur Wikifine</li></a>
         <a href=<?php echo FILE_PREFIX."pages/donate.php";?>><li class="navSubTitle">Faire un don</li></a>
     </ul>
 </nav>
