2c89c64eed07a678b1515ce023155a832dd9c5de
Leopold <leopold.goudier@gmail.com>
2023-07-03 16:59:56 +0200

    fix bug

--- pages/templates/navbar.php
+++ pages/templates/navbar.php
@@ -28,13 +28,13 @@
                 </a>
                 <div class="avatarDiv">
                     <a href="<?php echo FILE_PREFIX . "pages/user/user_main.php"; ?>">
-                        <img src=<?php
+                        <img src="<?php
                         $result = getData(array('id'), $_SESSION['id']);
                         $connect = connectDB();
                         $query = $connect->query("SELECT avatar_link FROM ".DB_PREFIX."USER WHERE id=".$result[0]);
                         $profil_pic = $query->fetch()['avatar_link'];
                         echo $profil_pic;
-                        ?> alt="Avatar" class="avatar">
+                        ?>" alt="Avatar" class="avatar">
                     </a>
                     <?php
                     if (isVerify($_SESSION['id'])) {
