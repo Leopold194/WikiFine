ae089bdf6526e04e1da32553955c0e3e904a2d8f
yb <ylianbsila22@gmail.com>
2023-07-03 23:22:20 +0200

    help-page

---
+++ pages/aide.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+    require '../core/functions.php';
+?>
+
+<?php require '../conf.inc.php'; ?>
+<?php require 'templates/head.php'; ?>
+<?php require 'templates/navbar.php'; ?>
+
+    <title>WikiFine - Aide</title>
+    <link rel="stylesheet" type="text/css" href="../css/aide.css">
+
+    <div class="container">
+        <h1>WikiFine - Aide</h1>
+        
+        <div class="section">
+            <h2>Comment rechercher un article ?</h2>
+            <p>Pour rechercher un article sur WikiFine, utilisez la barre de recherche en haut de la page. Entrez simplement le terme ou le sujet que vous souhaitez rechercher et appuyez sur Entrée ou cliquez sur le bouton de recherche. Les résultats pertinents seront affichés.</p>
+        </div>
+        
+        <div class="section">
+            <h2>Comment créer un compte utilisateur ?</h2>
+            <p>Pour créer un compte utilisateur sur WikiFine, suivez ces étapes :
+            <ol>
+                <li>Cliquez sur le lien "S'inscrire" ou "Créer un compte" dans le coin supérieur droit de la page.</li>
+                <li>Remplissez le formulaire d'inscription avec les informations requises, telles que votre nom, votre adresse e-mail et un mot de passe.</li>
+                <li>Cliquez sur le bouton "S'inscrire" ou "Créer un compte" pour finaliser la création de votre compte.</li>
+                <li>Un e-mail de confirmation vous sera envoyé avec un lien d'activation. Cliquez sur ce lien pour activer votre compte.</li>
+                <li>Vous pouvez maintenant vous connecter avec vos identifiants sur la page de connexion.</li>
+            </ol>
+            </p>
+        </div>
+        
+        <div class="section">
+            <h2>Comment contribuer en ajoutant un nouvel article ?</h2>
+            <p>Pour contribuer à WikiFine en ajoutant un nouvel article, suivez ces étapes :
+            <ol>
+                <li>Connectez-vous à votre compte utilisateur.</li>
+                <li>Cliquez sur le lien "Créer un Article" dans le menu principal.</li>
+                <li>Renseignez les informations requises dans le formulaire de création d'article, telles que le titre, le contenu et les catégories.</li>
+                <li>Une fois terminé, cliquez sur le bouton "Soumettre" ou "Publier" pour ajouter l'article à WikiFine.</li>
+                <li>Votre nouvel article sera examiné par les modérateurs avant d'être publié.</li>
+            </ol>
+            </p>
+        </div>
+        
+        <div class="section">
+            <h2>Comment contacter le support technique ?</h2>
+            <p>Pour contacter le support technique de WikiFine, vous pouvez utiliser le formulaire de contact disponible sur la page "Contact". Assurez-vous de fournir des informations détaillées sur votre problème ou votre demande d'assistance.</p>
+        </div>
+    </div>
+
